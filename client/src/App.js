c99be7a534f1b7530d52bd3c6245993228294914
David Wu <david10608@gmail.com>
2018-03-08 00:11:59 +0100

    adapt App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,20 +2,20 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
-  state = {users: []}
+  state = {votes: []}
 
   componentDidMount() {
-    fetch('/users')
+    fetch('/getPoll')
       .then(res => res.json())
-      .then(users => this.setState({ users }));
+      .then(votes => this.setState({ votes }));
   }
 
   render() {
     return (
       <div className="App">
-        <h1>Users</h1>
-        {this.state.users.map(user =>
-          <div key={user.id}>{user.username}</div>
+        <h1>Votes</h1>
+        {this.state.votes.map(vote =>
+          <div key={vote.id}>{vote.vote.author} voted {vote.vote.vote}</div>
         )}
       </div>
     );
