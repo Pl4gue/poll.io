108c2dd9c3c4da213e22afd91c3b9c81348fbc29
David Wu <david10608@gmail.com>
2018-03-08 22:31:50 +0100

    eslint styling

--- client/src/components/PollsView.js
+++ client/src/components/PollsView.js
@@ -1,19 +1,24 @@
 import React, { Component } from 'react';
+import classNames from 'classnames';
 
 import '../styles/css/PollsView.css';
 
 export default class PollsView extends Component {
-    state = {votes: []}
+    state = {
+    	votes: []
+    }
 
     componentDidMount() {
-        fetch('/getPoll')
-        .then(res => res.json())
-        .then(votes => this.setState({ votes }));
-    }
-    
+    	fetch('/getPoll')
+    		.then(res => res.json())
+    		.then(votes => this.setState({
+    			votes
+    		}));
+	}
+	
 	render() {  
 		return(
-			<div className="App">
+			<div className={classNames('App')}>
 				<h1>Votes</h1>
 				{this.state.votes.map(vote=>
 					<div key={vote.id}>{vote.vote.author} voted {vote.vote.vote}</div>
