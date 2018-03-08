0ae7f01496a6784ddae1870b213485163bfe2865
David Wu <david10608@gmail.com>
2018-03-09 00:25:53 +0100

    sample fetch OS Vote poll

--- client/src/components/DashboardView.js
+++ client/src/components/DashboardView.js
@@ -7,19 +7,6 @@ import Paper from 'material-ui/Paper';
 
 import '../styles/css/DashboardView.css';
 
-const data = {
-	labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-	datasets: [{
-		label: 'My First dataset',
-		backgroundColor: 'rgba(255,99,132,0.2)',
-		borderColor: 'rgba(255,99,132,1)',
-		borderWidth: 1,
-		hoverBackgroundColor: 'rgba(255,99,132,0.4)',
-		hoverBorderColor: 'rgba(255,99,132,1)',
-		data: [65, 59, 80, 81, 56, 55, 40]
-	}]
-};
-
 export default class DashboardView extends Component {
     state = {
     	votes: []
@@ -30,10 +17,37 @@ export default class DashboardView extends Component {
     		.then(res => res.json())
     		.then(votes => this.setState({
     			votes
-    		}));
+			}));
 	}
 	
 	render() {  
+		let [t1, t2, t3] = [0, 0, 0]
+		this.state.votes.forEach(item => {
+			switch (item.vote.vote) {
+				case "Windows":
+					t1++;
+					break;
+				case "Linux":
+					t2++;
+					break;
+				case "OS X":
+					t3++;
+					break;
+			}
+		});
+
+		const data = {
+			labels: ['Windows', 'Linux', 'OS X'],
+			datasets: [{
+				label: 'OS Poll',
+				backgroundColor: ['#00b894', '#00cec9', '#6c5ce7'],
+				borderWidth: 1,
+				hoverBackgroundColor: ['#55efc4', '#81ecec', '#a29bfe'],
+				hoverBorderColor: '#dfe6e9',
+				data: [t1,t2,t3],
+			}]
+		};
+
 		return(
 			<div className={classNames('root')}>
 				<Grid container spacing={24} direction={'row'}>
@@ -45,6 +59,15 @@ export default class DashboardView extends Component {
 									width={100}
 									height={50}
 									options={{
+										scales: {
+											yAxes: [{
+												display: true,
+												ticks: {
+													beginAtZero: true,
+													suggestedMax: 20,
+												}
+											}]
+										},
 										maintainAspectRatio: false
 									}}
 								/>
