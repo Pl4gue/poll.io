9eaf4506742068fd0ad4203a5b0a1b37cefb2921
David Wu <david10608@gmail.com>
2018-03-08 23:37:47 +0100

    Wrap ContentViews in root container

--- client/src/components/AboutView.js
+++ client/src/components/AboutView.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import classNames from 'classnames';
 
 import '../styles/css/AboutView.css';
 
 export default class AboutView extends React.Component {
 	render() {
 		return(
-			<h1>About</h1>
+			<div className={classNames('root')}>
+				<h1>About</h1>
+			</div>
 		);
 	}
 }
