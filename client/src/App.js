e7a7dc1361a7bfcadd40117ec80fc1fc9cff2b81
David Wu <david10608@gmail.com>
2018-03-08 01:21:58 +0100

    outsource to Drawer

--- client/src/App.js
+++ client/src/App.js
@@ -3,25 +3,11 @@ import './App.css';
 import PersistentDrawer from './components/PersistentDrawer';
 
 class App extends Component {
-  state = {votes: []}
-
-  componentDidMount() {
-    fetch('/getPoll')
-      .then(res => res.json())
-      .then(votes => this.setState({ votes }));
-  }
-
-  render() {
-    return (
-      // <div className="App">
-      //   <h1>Votes</h1>
-      //   {this.state.votes.map(vote=>
-      //     <div key={vote.id}>{vote.vote.author} voted {vote.vote.vote}</div>
-      //   )}
-      // </div> 
-      <PersistentDrawer/>
-    );
-  }
+	render() {
+		return (
+			<PersistentDrawer />
+		);
+	}
 }
 
 export default App;
