e0f6271be73c30b6ed8dc0739bcefb4610c7c222
David Wu <david10608@gmail.com>
2018-03-09 23:32:39 +0100

    eslint err fix

--- models/vote.js
+++ models/vote.js
@@ -1,18 +1,18 @@
-const mongoose = require('mongoose')
-const Schema = monoose.Schema
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
 const VoteSchema = new Schema({
-  VoteMeta: { // This holds general info about the votes
-    PollId: Number, // id of the poll
-    Username: String // If availible e.g. logged in (not a necessity)
-  },
-  VoteDetail: [{ // Array of votes (infinite size)
-    QuestionId: Number, // Id of the question
-    Answers: [{ AnswerId: Number }] // Id of the answer(s)
-  }]
-})
+	VoteMeta: { // This holds general info about the votes
+		PollId: Number, // id of the poll
+		Username: String // If availible e.g. logged in (not a necessity)
+	},
+	VoteDetail: [{ // Array of votes (infinite size)
+		QuestionId: Number, // Id of the question
+		Answers: [{ AnswerId: Number }] // Id of the answer(s)
+	}]
+});
 
 // Create collection and add Schema
-const Vote = mongoose.model('Vote', VoteSchema)
+const Vote = mongoose.model('Vote', VoteSchema);
 
-module.exports = Vote
+module.exports = Vote;
