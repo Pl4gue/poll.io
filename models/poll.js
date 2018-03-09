1cc3a5d0be8b0dcaf19da9a8326c8172c7371cd1
Quentin Oschatz <quentin@cliffhouse.de>
2018-03-09 23:23:43 +0100

    Added QuestionType to collections

--- models/poll.js
+++ models/poll.js
@@ -12,6 +12,7 @@ const PollSchema = new Schema({
   Questions: [{ // Array of questions (infinite size)
     QuestionId: Number, // Id of the question
     Question: String, // The text of the question
+    QuestionType: String, // The type of question, e.g. multi-select
     Answers: [{ // Array of all answers to that question
       AnswerId: Number, // Id of the answer
       Answer: String // The text of the answer
