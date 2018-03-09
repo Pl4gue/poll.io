f8b02081ce5655a609a458ee53c00bf6ec18825e
Quentin Oschatz <quentin@cliffhouse.de>
2018-03-09 23:19:05 +0100

    Added collections for db

---
+++ models/vote.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose')
+const Schema = monoose.Schema
+
+const VoteSchema = new Schema({
+  VoteMeta: { // This holds general info about the votes
+    PollId: Number, // id of the poll
+    Username: String // If availible e.g. logged in (not a necessity)
+  },
+  VoteDetail: [{ // Array of votes (infinite size)
+    QuestionId: Number, // Id of the question
+    Answers: [{ AnswerId: Number }] // Id of the answer(s)
+  }]
+})
