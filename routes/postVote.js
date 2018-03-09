053c92cee261786441a04c308b0553eb87960b48
Quentin Oschatz <quentin@cliffhouse.de>
2018-03-10 00:31:54 +0100

    Added routes and db connection

--- routes/postVote.js
+++ routes/postVote.js
@@ -1,24 +1,20 @@
 var express = require('express');
-// const Pusher = require('pusher');
 var router = express.Router();
+const mongoose = require('mongoose');
 
-/* // Pusher
-var pusher = new Pusher({
-	appId: '485099',
-	key: '279e1b477e33bd9e19a4',
-	secret: '823fc6980ff75283010c',
-	cluster: 'eu',
-	encrypted: true
-}); */
+const Vote = require('../models/vote.js');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-	/* pusher.trigger('poll', 'voting', {
-		votes: req.body.votes
-	}); */
+router.post('/postvote', function(req, res, next) {
+	const newVote = {
+		req.body
+	}
 
-	console.log(req);
-	// TODO: Implement pushing vote to database
+	new Vote(newVote).save().then(vote => {
+		//TODO: trigger socket.io
+		// Successfully added nre vote to db
+		console.log(vote);
+	});
 
 	return res.json({ success: true, message: 'Your votes have been proccessed. Thank you!' });
 });
