c900549eeb6998cc01c974e172ce7af3a4eae2f7
David Wu <david10608@gmail.com>
2018-03-09 23:32:39 +0100

    eslint err fix

--- models/poll.js
+++ models/poll.js
@@ -1,26 +1,26 @@
-const mongoose = require('mongoose')
-const Schema = monoose.Schema
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
 const PollSchema = new Schema({
-  PollMeta: { // This holds general info about the poll
-    PollId: Number, // Id of the poll
-    Author: String, // Username of the person creating the poll
-    PollTitle: String, // The title of the poll
-    PollDescription: String, // Optional poll description
-    PollThankYou: String // Optional 'Thank you' message to be displayed on completion
-  },
-  Questions: [{ // Array of questions (infinite size)
-    QuestionId: Number, // Id of the question
-    Question: String, // The text of the question
-    QuestionType: String, // The type of question, e.g. multi-select
-    Answers: [{ // Array of all answers to that question
-      AnswerId: Number, // Id of the answer
-      Answer: String // The text of the answer
-    }]
-  }]
-})
+	PollMeta: { // This holds general info about the poll
+		PollId: Number, // Id of the poll
+		Author: String, // Username of the person creating the poll
+		PollTitle: String, // The title of the poll
+		PollDescription: String, // Optional poll description
+		PollThankYou: String // Optional 'Thank you' message to be displayed on completion
+	},
+	Questions: [{ // Array of questions (infinite size)
+		QuestionId: Number, // Id of the question
+		Question: String, // The text of the question
+		QuestionType: String, // The type of question, e.g. multi-select
+		Answers: [{ // Array of all answers to that question
+			AnswerId: Number, // Id of the answer
+			Answer: String // The text of the answer
+		}]
+	}]
+});
 
 // Create collection and add Schema
-const Poll = mongoose.model('Poll', PollSchema)
+const Poll = mongoose.model('Poll', PollSchema);
 
-module.exports = Poll
+module.exports = Poll;
