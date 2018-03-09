08fc52652185842805c300a1fef7115ae4f169ae
David Wu <david10608@gmail.com>
2018-03-09 18:57:57 +0100

    comment out pusher

--- routes/postVote.js
+++ routes/postVote.js
@@ -1,21 +1,21 @@
 var express = require('express');
-const Pusher = require('pusher');
+// const Pusher = require('pusher');
 var router = express.Router();
 
-// Pusher
+/* // Pusher
 var pusher = new Pusher({
 	appId: '485099',
 	key: '279e1b477e33bd9e19a4',
 	secret: '823fc6980ff75283010c',
 	cluster: 'eu',
 	encrypted: true
-});
+}); */
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-	pusher.trigger('poll', 'voting', {
+	/* pusher.trigger('poll', 'voting', {
 		votes: req.body.votes
-	});
+	}); */
 
 	console.log(req);
 	// TODO: Implement pushing vote to database
