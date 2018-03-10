bd9a9c8b7945e930d42a1fd92ddc5895635f58d4
David Wu <david10608@gmail.com>
2018-03-10 12:54:26 +0100

    add a poll- and a detailstile

---
+++ client/src/components/dashboard/PollTile.js
@@ -0,0 +1,67 @@
+import React from 'react';
+import {Bar} from 'react-chartjs-2';
+
+export default class PollTile extends React.Component {
+    state = {
+    	votes: []        
+    }
+
+    componentDidMount() {
+    	fetch('/getPoll')
+    		.then(res => res.json())
+    		.then(votes => this.setState({
+    			votes
+			}));
+	}
+    
+    render() {
+        let [t1, t2, t3] = [0, 0, 0]
+		this.state.votes.forEach(item => {
+			switch (item.vote.vote) {
+				case "Windows":
+					t1++;
+					break;
+				case "Linux":
+					t2++;
+					break;
+				case "OS X":
+					t3++;
+					break;
+				default:
+					break;
+			}
+		});
+
+		const data = {
+			labels: ['Windows', 'Linux', 'OS X'],
+			datasets: [{
+				label: 'OS Poll',
+				backgroundColor: ['#00b894', '#00cec9', '#6c5ce7'],
+				borderWidth: 1,
+				hoverBackgroundColor: ['#55efc4', '#81ecec', '#a29bfe'],
+				hoverBorderColor: '#dfe6e9',
+				data: [t1,t2,t3],
+			}]
+        };
+        
+		return(
+            <Bar
+                data={data}
+                width={100}
+                height={50}
+                options={{
+                    scales: {
+                        yAxes: [{
+                            display: true,
+                            ticks: {
+                                beginAtZero: true,
+                                suggestedMax: 20,
+                            }
+                        }]
+                    },
+                    maintainAspectRatio: false
+                }}
+            />
+		);
+	}
+}
